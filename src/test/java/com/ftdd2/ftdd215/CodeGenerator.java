fd61af7ef6143b356b4f23b7898626169d615882
ziyi0227 <1206881089@qq.com>
2024-02-07 16:55:08 +0800

    代码生成器修改（还差表格）

--- src/test/java/com/ftdd2/ftdd215/CodeGenerator.java
+++ src/test/java/com/ftdd2/ftdd215/CodeGenerator.java
@@ -39,8 +39,8 @@ public class CodeGenerator {
                             .pathInfo(Collections.singletonMap(OutputFile.xml, mapperLocation)); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("t_simple") // 设置需要生成的表名
-                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
+                    builder.addInclude(tables); // 设置需要生成的表名
+                            //.addTablePrefix(); // 设置过滤表前缀
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
