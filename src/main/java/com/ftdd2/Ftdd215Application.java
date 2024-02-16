8a6bcf3ec9eab030c07aab82e706b0c3afcce1ee
ziyi0227 <1206881089@qq.com>
2024-02-16 22:58:05 +0800

    配置mybatis，修改依赖bug

--- src/main/java/com/ftdd2/Ftdd215Application.java
+++ src/main/java/com/ftdd2/Ftdd215Application.java
@@ -1,9 +1,11 @@
 package com.ftdd2;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ftdd2.*.mapper")
 public class Ftdd215Application {
 
     public static void main(String[] args) {
