fee15bd626f5b8ab729c20afc541ab129d5b0c39
ziyi0227 <1206881089@qq.com>
2024-02-21 22:35:38 +0800

    配置springdoc（文档打不开，找不到在哪）

---
+++ src/main/java/com/ftdd2/config/SpringDocConfig.java
@@ -0,0 +1,28 @@
+package com.ftdd2.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 接口文档配置
+ *
+ * @author Canva
+ */
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info().title("后端接口文档").version("1.0.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi httpApi() {
+        return GroupedOpenApi.builder()
+                .group("http")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
--- src/test/java/com/ftdd2/ftdd215/JwtUtilTest.java
+++ src/test/java/com/ftdd2/ftdd215/JwtUtilTest.java
@@ -11,22 +11,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class JwtUtilTest {
 
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Test
-    public void testCreateJwt(){
-        Users user = new Users();
-        user.setUsername("admin");
-        user.setPhone("123456");
-        String jwt = jwtUtil.createToken(user);
-        System.out.println(jwt);
-    }
-
-    @Test
-    public void testParesJwt(){
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0MzkyMzA0OS03NjgxLTQ0MjctOThhZS1kODhmYmU0NTVkZjkiLCJzdWIiOiJ7XCJwaG9uZVwiOlwiMTIzNDU2Nzg5XCIsXCJ1c2VybmFtZVwiOlwiemhhbmdzYW5cIn0iLCJpc3MiOiJzeXN0ZW0iLCJpYXQiOjE3MDg1MDY0ODksImV4cCI6MTcwODUxNzI4OX0.AbBROAo-7MJbILMkI7nYNdiXwz48FaX2Dps3WPx9njY";
-        Claims claims = jwtUtil.parseToken(token);
-        System.out.println(claims);
-    }
+    // @Autowired
+    // private JwtUtil jwtUtil;
+    //
+    // @Test
+    // public void testCreateJwt(){
+    //     Users user = new Users();
+    //     user.setUsername("admin");
+    //     user.setPhone("123456");
+    //     String jwt = jwtUtil.createToken(user);
+    //     System.out.println(jwt);
+    // }
+    //
+    // @Test
+    // public void testParesJwt(){
+    //     String token = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0MzkyMzA0OS03NjgxLTQ0MjctOThhZS1kODhmYmU0NTVkZjkiLCJzdWIiOiJ7XCJwaG9uZVwiOlwiMTIzNDU2Nzg5XCIsXCJ1c2VybmFtZVwiOlwiemhhbmdzYW5cIn0iLCJpc3MiOiJzeXN0ZW0iLCJpYXQiOjE3MDg1MDY0ODksImV4cCI6MTcwODUxNzI4OX0.AbBROAo-7MJbILMkI7nYNdiXwz48FaX2Dps3WPx9njY";
+    //     Claims claims = jwtUtil.parseToken(token);
+    //     System.out.println(claims);
+    // }
 }
